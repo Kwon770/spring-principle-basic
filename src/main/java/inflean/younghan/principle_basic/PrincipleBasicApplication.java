e502dbba113d1356e3213d2455b738565966c9b8
Kwon770 <sckwon770@gmail.com>
2023-06-02 23:03:08 +0900

    chore: Init a project

---
+++ src/main/java/inflean/younghan/principle_basic/PrincipleBasicApplication.java
@@ -0,0 +1,13 @@
+package inflean.younghan.principle_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincipleBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincipleBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inflean/younghan/principle_basic/PrincipleBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package inflean.younghan.principle_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincipleBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
