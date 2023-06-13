49d8c07f6d271a33469cf949aa22b4c5ee28d48a
Kwon770 <sckwon770@gmail.com>
2023-06-13 19:18:13 +0900

    section5: start~end

---
+++ src/test/java/inflean/younghan/principle_basic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,39 @@
+package inflean.younghan.principle_basic.singleton;
+
+import inflean.younghan.principle_basic.AppConfig;
+import inflean.younghan.principle_basic.member.MemberRepository;
+import inflean.younghan.principle_basic.member.MemberServiceImpl;
+import inflean.younghan.principle_basic.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = memberService.getMemberRepository();
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
+}
